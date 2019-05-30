da109a11a4d628ca60e417d1a7a9858020b214f4
Lauren Granger <helloworld@laurengranger.com>
2019-05-30 15:01:59 -0700

    update Hamming to use one line for zip and count

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -2,11 +2,5 @@ package Hamming
 
     public fun compute(strandA: String, strandB: String): Number {
         require(strandA.length === strandB.length) { "left and right strands must be of equal length." }
-        val zipList = strandA.zip(strandB) { a, b -> diff(a, b)}
-        return zipList.count { a -> a.equals("") }
-    }
-
-    private fun diff(charA: Char, charB: Char): String {
-        if (charA.equals(charB)) { return "charA" }
-        return ""
+        return strandA.zip(strandB).count { it.first != it.second }
     }
